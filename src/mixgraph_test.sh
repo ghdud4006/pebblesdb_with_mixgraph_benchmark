79fcfa3c5906d9ddfe129cd003729422fc811bcd
young <ghdud4006@gmail.com>
2020-06-04 21:08:17 +0900

    Update MixGraph workload to PebblesDB

---
+++ src/mixgraph_test.sh
@@ -0,0 +1,2 @@
+./db_bench --benchmarks="mixgraph,stats" --keyrange_dist_a=14.18 --keyrange_dist_b=-2.917 --keyrange_dist_c=0.0164 --keyrange_dist_d=-0.08082 --keyrange_num=30 --value_k=0.2615 --value_sigma=25.45 --iter_k=2.517 --iter_sigma=14.236 --mix_get_ratio=0 --mix_put_ratio=1 --mix_seek_ratio=0 --num=10000000 --key_size=48 --db=/home/young/mnt_nvme
+./db_bench --benchmarks="mixgraph,stats" --num=10000000 --key_size=48 --db=/home/young/mnt_nvme
